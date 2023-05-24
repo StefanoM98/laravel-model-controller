486ef559167437bc79542184fe443e7375af7028
StefanoM98 <smartino030@gmail.com>
2023-05-24 14:34:43 +0200

    Add: Details with Sass

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Questa è Boolean Mondadori</h2>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>
-                <img src="{{ $movie->image }}" alt="">
-                <h2>{{ $movie->title }}</h2>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container text-center">
+        <h2>Questa è Boolean Mondadori</h2>
+        <div class="row row-cols-4">
+            @foreach ($movies as $movie)
+                <div class="col p-4">
+                    <div class="card">
+                        <figure>
+                            <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-text">{{ $movie->title }}</h2>
+                            <p class="card-text">Voto: {{ $movie->vote }}</p>
+                            <p class="card-text">Data di uscita: {{ $movie->date }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
